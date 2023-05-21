3f5053261d008a9a8215e925fe484403b7bd846d
ImJoke <55929550+ImJoke@users.noreply.github.com>
2023-05-21 23:39:44 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,49 @@
+# Install
+```
+git clone https://github.com/ImJoke/nmap-initial-scan
+mv nmap-initial-scan/initial.py .
+rm -rf nmap-initial-scan
+```
+# Usage
+```sh
+python initial.py 192.168.0.1 -s
+```
+```sh
+python initial.py 192.168.0.1 -s -v -Pn -p- --min-rate 5000
+```
+```sh
+python initial.py 192.168.0.1 -s -vvv --min-rate 5000
+```
+Let's assume our gateway is `192.168.0.1` and we want to scan 192.168.0.100 but too lazy to write `192.168.0.x` then we can just do
+```sh
+python initial.py 100
+```
+If you want to get the details you can simply put `-s`
+```sh
+python initial.py 100 -s
+```
+# Help output
+```sh
+$ python initial.py -h
+usage: usage: initial.py [-h] [-p <1-65535>] [-T<0-5>] [-v] [-s] [-Pn] [--min-rate <number>] IP
+
+Modern initial scan with nmap
+
+positional arguments:
+  IP             Target IP address
+
+options:
+  -h, --help     show this help message and exit
+  -p, --port     Port
+  -T<0-5>        Set timing template (higher is faster)
+  -v, --verbose  Verbose
+  -s, --status   Get status information for debugging
+  -Pn            Treat all hosts as online -- skip host discovery
+  --min-rate     Send packets no slower than <number> per second
+  -o, --output   Output path
+```
+# Supported version
+Python ≥ 3.x.x
+
+#  TODO
+- [ ] Support for domain
---
+++ initial.py
@@ -0,0 +1,52 @@
+import argparse, ipaddress
+from os import system, popen
+
+class CustomFormatter(argparse.HelpFormatter):
+    def _format_action_invocation(self, action):
+        if not action.option_strings:
+            metavar, = self._metavar_formatter(action, action.dest)(1)
+            return metavar
+        else:
+            parts = []
+
+            if action.nargs == 0:
+                parts.extend(action.option_strings)
+            else:
+                default = action.dest.upper()
+                args_string = self._format_args(action, default)
+                for option_string in action.option_strings:
+                    parts.append(option_string)
+                parts[-1] += args_string
+            return ', '.join(parts)
+
+parser = argparse.ArgumentParser(usage='usage: initial.py [-h] [-p <1-65535>] [-T<0-5>] [-v] [-s] [-Pn] [--min-rate <number>] IP', formatter_class=CustomFormatter, description='Modern initial scan with nmap')
+parser.add_argument('IP', help='Target IP address')
+parser.add_argument('-p', '--port', type=int, metavar='', help='Port to scan')
+parser.add_argument('-T', action='store', type=int, metavar='<0-5>', help='Set timing template (higher is faster)')
+parser.add_argument('-v', '--verbose', action='count', help='Verbose')
+parser.add_argument('-s', '--status', action='store_true', help='Get status information for debugging')
+parser.add_argument('-Pn', action='store_true', help='Treat all hosts as online -- skip host discovery')
+parser.add_argument('--min-rate', action='store', type=int, metavar='', help='Send packets no slower than <number> per second')
+parser.add_argument('-o', '--output', action='store', type=str, metavar='', default='/tmp/nmap/', help='Output path')
+
+args = parser.parse_args()
+gateway = popen("ip route | awk '/default/ { print $3 }' | head -n 1").read().strip()
+
+def checker(ip) -> bool:
+    return all(map(lambda x: int(x) >= 0 and int(x) <= 255, ip.split('.')))
+
+args.IP = args.IP if checker(args.IP) and args.IP.count('.') == 3 else gateway.rsplit('.', 1)[0] + '.' + args.IP if checker(args.IP) else exit('Invalid IP address')
+args.port = '' if args.port == None else f' -p {args.port}' if args.port != 0 else ' -p-'
+args.T = f' -T{args.T}' if args.T else ''
+args.verbose = ' -'+''.join(['' if args.verbose == None else 'v' for _ in range(args.verbose)]) if args.verbose else ''
+args.Pn = ' -Pn' if args.Pn else ''
+args.min_rate = '' if args.min_rate == None else f' --min-rate {args.min_rate}'
+args.output = args.output if args.output.endswith('/') else args.output + '/'
+
+if args.status:
+    print(f'Private network' if ipaddress.ip_address(args.IP).is_private else 'Public network' if ipaddress.ip_address(args.IP).is_global else '')
+    print(f"IP: {args.IP} Port: {args.port.replace(' -p ', '') if args.port != ' -p-' else '1-65535'}")
+    print(f'Exec: {"sudo nmap -sV -sC -O"+args.port+args.T+args.verbose+args.Pn+args.min_rate +" -oA "+ args.output+args.IP+" "+args.IP!r}')
+    print('Web: http://' + args.IP, '| https://' + args.IP)
+
+system(fr"sudo nmap -sV -sC -O{args.port}{args.verbose}{args.Pn}{args.min_rate} -oA {args.output}{args.IP} {args.IP}")
